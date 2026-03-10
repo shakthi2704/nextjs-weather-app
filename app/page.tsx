017571d146f173bc4f4239633fcc0032c44f26e4
shakthi <shakthi2704@gmail.com>
2026-03-10 19:59:17 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-3xl">
+      <h1 className="text-5xl font-bold">Hello, Weather App!</h1>
+    </div>
+  );
+}
